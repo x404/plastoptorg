cf135ad64f4a7ee0ae9438658757807f160efe35
x404 <kronos2003@gmail.com>
2018-01-09 21:30:21 +0200

    Сделано прикрепление файлов в форме обратной связи на главной

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -195,3 +195,9 @@ ymaps.ready(function () {
 function go_to(lat,lon){
 	map.setCenter([lat, lon], 16);
 }
+
+function getFileName() {
+	var file = document.getElementById('uploaded-file').value;
+	file = file.replace(/\\/g, "/").split ('/').pop();
+	document.getElementById('file-name').innerHTML = 'Имя файла: ' + file;
+}
