753fb3164d803fe3f172fe4771e38a637b4b54bc
x404 <kronos2003@gmail.com>
2018-01-09 22:20:15 +0200

    Сделано выпадающее меню, добавлена fancybox

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -196,7 +196,7 @@ function go_to(lat,lon){
 	map.setCenter([lat, lon], 16);
 }
 
-function getFileName() {
+function getFileName(){
 	var file = document.getElementById('uploaded-file').value;
 	file = file.replace(/\\/g, "/").split ('/').pop();
 	document.getElementById('file-name').innerHTML = 'Имя файла: ' + file;
